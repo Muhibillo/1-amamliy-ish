8e262493bbde5c42d200f1b91461c333c0df052d
Muhibillo <113684591+Muhibillo@users.noreply.github.com>
2022-09-22 13:16:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+x1=int(input("x1 ni kiriting:"))
+y1=int(input("y1 ni kiriting:"))
+x2=int(input("x2 ni kiriting:"))
+y2=int(input("y2 ni kiriting:"))
+c=(x2-x1)**2
+d=(y2-y1)**2
+D=math.sqrt(c+d)
+print(f"ikki nuqta orasidagi masofa={D}")
